b7ac3d7704b02e14927617df1846503fe960664a
Jade-W997 <flickerww997@gmail.com>
2026-01-29 23:03:51 +0900

    simple_analysis.py

---
+++ simple_analysis.py
@@ -0,0 +1,8 @@
+# Simple data analysis example
+# This script calculates the average value from a list of user interaction times
+
+interaction_times = [2.3, 1.8, 3.1, 2.6, 1.9]
+
+average_time = sum(interaction_times) / len(interaction_times)
+
+print("Average interaction time:", average_time)
